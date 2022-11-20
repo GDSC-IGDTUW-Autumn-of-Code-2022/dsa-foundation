6f5bf85eb9cc47bc723110ae040e7dcc4d887a7c
Monachaudhary <manuchaudhary991@gmail.com>
2022-11-20 18:17:36 +0530

    Solution to the balanced parenthesis using java

--- Balanced_Parenthesis.java
+++ Balanced_Parenthesis.java
@@ -1,21 +1,55 @@
 public class Balanced_Parenthesis {
     public static void main(String[] args) {
-        String parenthesis = "}}}";
-        int result = balanced_Parenthesis(parenthesis);
-        System.out.println(result);
+        String parenthesis = "}{";
+        int res = balanced_Parenthesis(parenthesis);
+        System.out.println(res);
 
     }
 
-    public static int balanced_Parenthesis(String s) {
-        int count = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '{' || s.charAt(i) == '}') {
-                count++;
+    public static int balanced_Parenthesis(String inputStr) {
+        int i, length, j = 0, count = 0;
+        int counter = 0;
+        char current, ch;
+        char[] stack = new char[20];
+        length = inputStr.length();
+        for (i = 0; i < length; i++) {
+
+            current = inputStr.charAt(i);
+            if (current == '(' || current == '{' || current == '[') {
+                stack[j] = current;
+                j++;
+                count = 1;
+                counter++;
+            } else if (current == ')') {
+                if (count == 1) {
+                    j--;
+                    counter--;
+                }
+                ch = stack[j];
+                if (stack.length == 0 || ch != '(') {
+                    counter++;
+                }
+            } else if (current == '}') {
+                if (count == 1) {
+                    j--;
+                    counter--;
+                }
+                ch = stack[j];
+                if (stack.length == 0 || ch != '{') {
+                    counter++;
+                }
+            } else if (current == ']') {
+                if (count == 1) {
+                    j--;
+                    counter--;
+                }
+                ch = stack[j];
+                if (stack.length == 0 || ch != '[') {
+                    counter++;
+                }
             }
-
         }
-        return count;
 
+        return counter;
     }
-
 }
