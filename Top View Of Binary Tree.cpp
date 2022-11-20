1c59afda9fb26b492d148e2b6a2dac3c955ca8f7
gh4ahbi <gh4abhi@gmail.com>
2022-11-21 05:04:12 +0530

    Added solution of one problem.

--- Top View Of Binary Tree.cpp
+++ Top View Of Binary Tree.cpp
@@ -2,9 +2,12 @@
 
 #include<bits/stdc++.h>
 #define ll int
-/************************************************************
 
-    Following is the TreeNode class structure:
+using namespace std;
+
+// ***********************************************************
+
+// Following is the TreeNode class structure:
 
     template <typename T>
     class TreeNode {
@@ -20,7 +23,7 @@
         }
     };
 
-************************************************************/
+// ***********************************************************
 
 vector<int> getTopView(TreeNode<int> *root) {
     map<ll,ll> m;
@@ -43,4 +46,50 @@ vector<int> getTopView(TreeNode<int> *root) {
     for(auto i:m)
         ans.push_back(i.second);
     return ans;
+}
+
+TreeNode<ll>* createTree()
+{
+    queue<TreeNode<ll>*> q;
+    TreeNode<ll> *root = nullptr;
+    ll val;
+    cout<<"Enter value of root - "<<endl;
+    cin>>val;
+    root = new TreeNode<ll>(val);
+    q.push(root);
+    while(q.size())
+    {
+        auto cur = q.front();
+        q.pop();
+        ll lef,rig;
+        cout<<"Enter left child of "<<cur->val<<endl;
+        cin>>lef;
+        cout<<"Enter right child of "<<cur->val<<endl;
+        cin>>rig;
+        if(lef==-1)
+            cur->left = nullptr;
+        else
+        {
+            TreeNode<ll> *l = new TreeNode<ll>(lef);
+            cur->left = l; 
+            q.push(l);
+        }
+        if(rig==-1)
+            cur->right = nullptr;
+        else
+        {
+            TreeNode<ll> *r = new TreeNode<ll>(rig);
+            cur->right = r;
+            q.push(r);
+        }
+    }
+    return root;
+}
+
+int main()
+{
+    TreeNode<ll> *root = createTree();
+    vector<ll> ans = getTopView(root);
+    for(auto i:ans)
+        cout<<i<<" ";
 }
