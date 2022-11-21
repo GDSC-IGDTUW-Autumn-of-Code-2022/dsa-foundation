aca5eecb1b623becaefa41df01b2d2e036476c8d
Pratiksha Goswami <104529563+Pratikshathisside@users.noreply.github.com>
2022-11-21 23:09:13 +0530

    Create kth_largest_element_in_array.cpp

---
+++ kth_largest_element/kth_largest_element_in_array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        
+  
+    sort(nums.begin(),nums.end());
+    int i=nums.size();
+    return nums[i-k];
+
+        
+    }
+};
