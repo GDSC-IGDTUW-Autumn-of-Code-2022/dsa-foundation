ab5f25bdb66395b312eb48d1ce2b6ac7b32ee233
Tanisha Bansal <100759293+tanisha12j@users.noreply.github.com>
2022-12-03 17:26:36 +0530

    Update powerset.cpp

--- Power set/powerset.cpp
+++ Power set/powerset.cpp
@@ -1,1 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+vector<string> AllPossibleStrings(string s) {
+	int n = s.length();
+	vector<string>ans;
+	for (int num = 0; num < (1 << n); num++) {
+		string sub = "";
+		for (int i = 0; i < n; i++) {
+			//check if the ith bit is set or not
+			if (num & (1 << i)) {
+				sub += s[i];
+			}
+		}
+		if (sub.length() > 0) {
+			ans.push_back(sub);
+		}
+	}
+	sort(ans.begin(), ans.end());
+	return ans;
+}
+int main()
+{
+
+
+	string s;
+  cout<<"Enter the string"<<endl;
+  cin>>s;
+	vector<string>ans = AllPossibleStrings(s);
+	//printint all the subsequence.
+	cout<<"All possible subsequences are "<<endl;
+	for (auto it : ans) {
+		cout << it << " ";
+	}
+
+}
